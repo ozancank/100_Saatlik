ce12c3b10176213a2ddf494a400046862ef34552
ozancank <ozancank@gmail.com>
2020-04-01 22:30:57 +0300

    .

--- Python/dosyaOrnek.py
+++ Python/dosyaOrnek.py
@@ -1,12 +1,22 @@
 ogrenciler=["o1","o2","o3","o4","o5"]
 
 f=open("ogrenci.txt","a")
+print("Dosya Oluştu.")
+print("")
 
 for i in ogrenciler:
     f.write(i)
     f.write("\n")
+f.close()
 
+f=open("ogrenci.txt","r")
+print(f.read())
 f.close()
 
 import os
-os.remove("ogrenci.txt")
+
+if os.path.exists("ogrenci.txt"):
+    os.remove("ogrenci.txt")
+    print("Dosya silindi.")
+else:
+    print("Dosya yok.")
