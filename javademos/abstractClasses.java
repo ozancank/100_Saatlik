10b5f4b968ce245f4b3b4f67730c62db161072d6
ozancank <ozancank@gmail.com>
2020-04-03 23:34:28 +0300

    .

---
+++ javademos/abstractClasses.java
@@ -0,0 +1,8 @@
+public class abstractClasses {
+    public static void main(String[] args) {
+        //WomanGameCalculator womanGameCalculator=new WomanGameCalculator();
+        GameCalculator womanGameCalculator=new WomanGameCalculator();
+        womanGameCalculator.hesapla();
+        womanGameCalculator.gameOver();
+    }
+}
