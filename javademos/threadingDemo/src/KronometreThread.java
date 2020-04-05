d1cb116b654a096f924096c3634133425fcf5bae
ozancank <ozancank@gmail.com>
2020-04-05 15:46:14 +0300

    .

---
+++ javademos/threadingDemo/src/KronometreThread.java
@@ -0,0 +1,34 @@
+public class KronometreThread implements Runnable {
+
+    private Thread t;
+    private String threadName;
+
+    public KronometreThread(String threadName) {
+        this.threadName = threadName;
+        System.out.println("Oluşturuluyor : " + threadName);
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Çalıştırılıyor : " + threadName);
+
+        try {
+            for (int i = 0; i <= 10; i++) {
+                System.out.println(threadName + " : " + i);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Kesildi : " + threadName);
+            e.printStackTrace();
+        }
+        System.out.println("Thread bitti : " + threadName);
+    }
+
+    public void start() {
+        System.out.println("Thread nesnesi oluşuyor.");
+        if (t == null) {
+            t = new Thread(this, threadName);
+            t.start();
+        }
+    }
+}
