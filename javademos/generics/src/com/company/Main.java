d47187bf42d14c1f1af86996e7da5ead832b5bc0
ozancank <ozancank@gmail.com>
2020-04-05 15:00:44 +0300

    .

---
+++ javademos/generics/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyList<Customer> myList=new MyList<Customer>();
+        myList.add(new Customer());
+        System.out.println("Tamam");
+    }
+}
