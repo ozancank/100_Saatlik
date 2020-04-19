3b07eaba563a1e05fda418e2919c6b92feb93494
ozancank <ozancank@gmail.com>
2020-04-19 21:57:21 +0300

    .

--- Angular/shop/src/app/app-routing.module.ts
+++ Angular/shop/src/app/app-routing.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductComponent } from './product/product.component';
-
+import { ProductAddForms1Component } from './product/product-add-forms1/product-add-forms1.component';
+import { ProductAddForms2Component } from './product/product-add-forms2/product-add-forms2.component';
 
 const routes: Routes = [
-  {path:"products",component:ProductComponent},
-  {path:"",redirectTo:"products",pathMatch:"full"},
-  {path:"products/category/:categoryId",component:ProductComponent}
+  { path: 'products', component: ProductComponent },
+  { path: 'product-add-1', component: ProductAddForms1Component },
+  { path: 'product-add-2', component: ProductAddForms2Component },
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'products/category/:categoryId', component: ProductComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Angular/shop/src/app/app.module.ts
+++ Angular/shop/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
@@ -11,6 +11,8 @@ import { ProductComponent } from './product/product.component';
 import { ProductFilterPipe } from './product/product-filter.pipe';
 import { from } from 'rxjs';
 import { AlertifyService } from './services/alertify.service';
+import { ProductAddForms1Component } from './product/product-add-forms1/product-add-forms1.component';
+import { ProductAddForms2Component } from './product/product-add-forms2/product-add-forms2.component';
 
 @NgModule({
   declarations: [
@@ -18,13 +20,16 @@ import { AlertifyService } from './services/alertify.service';
     NavComponent,
     CategoryComponent,
     ProductComponent,
-    ProductFilterPipe
+    ProductFilterPipe,
+    ProductAddForms1Component,
+    ProductAddForms2Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [AlertifyService],
   bootstrap: [AppComponent]
